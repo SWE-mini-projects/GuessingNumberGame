e8a8ca9104b468e2232b726f0da135029601085c
Ashan_264 <ashan@gatech.edu>
2024-08-18 12:53:39 -0400

    initial guessing game

---
+++ script.js
@@ -0,0 +1,67 @@
+const game = {
+  title: "Guess the Number!",
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuesses: [],
+
+  
+  play: function () {
+    this.secretNum =
+      Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) +
+      this.smallestNum;
+     this.enableGuessing();
+  },
+
+  guessHandler: function () {
+    const guess = this.getGuess();
+    this.prevGuesses.push(guess);
+    this.renderMessage(guess);
+    if (guess === this.secretNum) {
+      alert(`Congrats! You've guessed the number in ${this.prevGuesses.length} attempts!`);
+      document.getElementById('guessAgain').style.display = 'none'; // Hide "Guess Again" button
+    } else {
+      this.enableGuessing(); // Re-enable guessing
+    }
+  },
+
+enableGuessing: function () {
+    document.getElementById('guessAgain').style.display = 'inline'; // Show "Guess Again" button
+  },
+
+getGuess: function () {
+  let guess;
+  while (true) {
+    guess = parseInt(prompt(`Please enter a guess between ${this.smallestNum} and ${this.biggestNum}:`));
+      if (!isNaN(guess) && guess >= this.smallestNum && guess <= this.biggestNum) {
+        return guess;
+      }
+    alert(`Invalid input, please enter a number between ${this.smallestNum} and ${this.biggestNum}.`);
+    }
+     // Return the valid guess
+  },
+
+
+renderMessage: function(guess) {
+  const messageE1 = document.getElementById('messages');
+  let message = `Your guess: ${guess} `;
+  if (guess < this.secretNum) {
+    message += `is too low!`;
+  } else if (guess > this.secretNum) {
+    message += `is too high!`;
+  } else {
+    message += `is correct!`;
+  }
+  messageE1.innerHTML = message;
+}
+};
+
+document.getElementById('startGame').addEventListener('click',() => { game.play();
+                                                    });
+
+document.getElementById('guessAgain').addEventListener('click', () => {
+  game.guessHandler(); // Handle the guess when "Guess Again" is clicked
+});
+
+  
+
